2c1f39c80bcbfe26a395a487c2618f352b030975
Midas <midasscheffers@gmail.com>
2025-11-11 20:24:40 +0100

    [add] basic printing and loading FEN

--- engine/main.py
+++ engine/main.py
@@ -1,6 +1,7 @@
 
 
 class Piece():
+    Empty = 0
     King = 1
     Pawn = 2
     Knight = 3
@@ -16,6 +17,20 @@ class Piece():
     
     def piece_type(p):
         return 7 & p
+
+    def char_to_piece(p:str):
+        color = Piece.White if p.isupper() else Piece.Black
+        char_to_type_dict = {"k":Piece.King, "p": Piece.Pawn, "n":Piece.Knight, "b":Piece.Bishop, "r":Piece.Rook, "q":Piece.Queen}
+        p_type = char_to_type_dict[p.lower()]
+        return color | p_type
+
+    def piece_to_char(p:int):
+        type_to_char_dict = {Piece.King:"k", Piece.Pawn:"p", Piece.Knight:"n", Piece.Bishop:"b", Piece.Rook:"r", Piece.Queen:"q", Piece.Empty:" "}
+        p_str = type_to_char_dict[Piece.piece_type(p)]
+        if Piece.piece_color(p) == Piece.White:
+            p_str = p_str.upper()
+        return p_str
+
 
 class Board():
 
@@ -50,16 +65,26 @@ class Board():
         else:
             self.ep_square = self.cord_to_index(ep_square)
         
+        self.squares = [0 for _ in range(64)]
         lines = state.split("/")
         for j,l in enumerate(lines):
-            
+            k = 0
+            for c in l:
+                if c.isnumeric():
+                    k+= int(c)
+                else:
+                    self.set_piece(Piece.char_to_piece(c), j*8+k)
+                    k+= 1
+    
+    def print(self):
+        for i,p in enumerate(self.squares):
+            print(Piece.piece_to_char(p),end="")
+            if i%8==7:
+                print()
 
 
 
 
 b = Board()
-print(b.squares)
-b.set_piece(Piece.White|Piece.Bishop, 3)
-print(b.squares)
-print(Piece.piece_type(b.squares[3]))
-
+# print(b.squares)
+b.print()
