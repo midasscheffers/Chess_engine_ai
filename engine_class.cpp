451b27b78c24e51cd50cdbda1842d3c75e379dbe
Midas <midasscheffers@gmail.com>
2025-11-24 17:49:30 +0100

    [add] removeing pieces and adding pieces to bit boards now efficient, basic move making and printing

--- engine_class.cpp
+++ engine_class.cpp
@@ -6,59 +6,13 @@
 using namespace std;
 
 
-// class Piece{
-//     public:
-//     // piece values
-//     const int Empty = 0;
-//     const int King = 1;
-//     const int Pawn = 2;
-//     const int Knight = 3;
-//     const int Bishop = 4;
-//     const int Rook = 5;
-//     const int Queen = 6;
 
-//     const int White = 8;
-//     const int Black = 16;
+int algebraic_to_sq(string alg_sq){
+    int x = alg_sq[0] - 'a';
+    int y = alg_sq[1] - '0';
+    return x+8*(8-y);
+}
 
-//     int pieceType = 0;
-//     int pieceColor = 0;
-//     Location at;
-//     Piece(int pt, int pc){
-//         pieceType = pt;
-//         pieceColor = pc;
-//     }
-//     Piece(){
-//         pieceType = 0;
-//         pieceColor = 0;
-//         at = Location(-1);
-//     }
-// };
-
-
-
-// class Location{
-//     public:
-//     int square;
-
-//     Location(int sq){
-//         square = sq;
-//     }
-
-//     Location(){
-//         square=-1;
-//     }
-
-//     pair<int,int> xy(){
-//         return make_pair(square/8, square%8);
-//     }
-
-//     int sq(){
-//         return square;
-//     }
-    
-
-
-// };
 
 
 
@@ -118,6 +72,9 @@ class Board{
     bool is_white_turn;
     vector<vector<bool>> casteling_rights;
     vector<int> ep_sq;
+    // vector<Move> moves_made;
+    int moves_counter;
+    int halfmoves_counter;
 
     unordered_map<char,int> char_to_piece = {
         {'K',0}, {'P', 1}, {'N',2}, {'B', 3}, {'R', 4}, {'Q', 5},
@@ -139,16 +96,29 @@ class Board{
 
 
     void set_piece(int p,int sq){
-        // cout << "Set piece " << p << ", at " << sq << endl;
-        // cout << bitboards[p];
         unsigned long long one = 1;
         bitboards[p] |= (one<<sq);
-        // cout << " to " << bitboards[p] << endl;
+        // update all_boards
+        empty_squares = ~((~empty_squares) | (one<<sq));
+        if (p<6){
+            all_white_pieces |= (one<<sq);
+        }
+        else{
+            all_black_pieces |= (one<<sq);
+        }
     }
 
     void remove_piece(int p, int sq){
         unsigned long long one = 1;
-        bitboards[p] = ~((~bitboards[p]) | (one<<sq)); 
+        bitboards[p] = ~((~bitboards[p]) | (one<<sq));
+        // update the all_boards
+        empty_squares |= one<<sq;
+        if (p<6){
+            all_white_pieces = ~((~all_white_pieces) | (one<<sq));
+        }
+        else{
+            all_black_pieces = ~((~all_black_pieces) | (one<<sq));
+        }
     }
 
 
@@ -164,7 +134,6 @@ class Board{
 
     void LoadFEN(string FEN){
         // cout << "Loading FEN: " << FEN << endl;
-        
         // cut string
         vector<string> data;
         for (int i=0; i<6; i++){
@@ -203,13 +172,25 @@ class Board{
         unordered_map<char,int> char_to_int_rights = {{'K', 0}, {'Q', 1}, {'k', 2}, {'q', 3}};
         for (int c = 0; c<data[2].length(); c++){
             char curr_char = data[2][c];
-            if (not curr_char == '-'){
+            if (curr_char != '-'){
                 rights[char_to_int_rights[curr_char]] = true;
             }
         }
         casteling_rights.push_back(rights);
         // ep_sq
+        if (data[3] != "-"){
+            ep_sq.push_back(algebraic_to_sq(data[3]));
+        }
+        else{
+            ep_sq.push_back(-1);
+        }
         // move_counters
+        char mc = data[5][0];
+        int imc = mc - '0';
+        char hmc = data[4][0];
+        int ihmc = hmc - '0';
+        moves_counter = imc;
+        halfmoves_counter = ihmc;
 
     }
 
@@ -222,6 +203,26 @@ class Board{
             }
         }
         // print castle rights
+        cout << "cr: ";
+        for (int i =0;i<4;i++){
+            cout << casteling_rights.back()[i] << ", ";
+        }
+        cout << "m: " << moves_counter << " hm: " << halfmoves_counter;
+        cout << " ep: " << ep_sq.back() << endl; 
+        
+    }
+
+    void print_bitboards(){
+        for (int i =0; i<12; i++){
+            cout << "Bitboard: " << i << endl;
+            BitBoard().print(bitboards[i]);
+        }
+        cout << "AW BB:" << endl;
+        BitBoard().print(all_white_pieces);
+        cout << "AB BB:" << endl;
+        BitBoard().print(all_black_pieces);
+        cout << "Empty:" << endl;
+        BitBoard().print(empty_squares);
     }
 
 
@@ -239,29 +240,34 @@ class Board{
 
     void make_move(Move m){
         // check non special moves first
-        if (not (m.is_castle or m.is_capture or m.is_ep)){
-
-        }
-        else if (m.is_castle){
-
-        }
-        else if (m.is_ep){
-
+        if (!(m.is_castle || m.is_capture || m.is_ep)){
+            int piece = piece_on_sq(m.start);
+            remove_piece(piece, m.start);
+            set_piece(piece, m.end);
         }
         else if (m.is_capture){
+            int piece = piece_on_sq(m.start);
+            int enemy_piece = piece_on_sq(m.end);
+            remove_piece(piece, m.start);
+            remove_piece(enemy_piece, m.end);
+            set_piece(piece, m.end);
+        }
+        else if (m.is_ep){
+            
+        }
+        else if (m.is_castle){
+            
+        }
+        // castle rights logic
 
+        // ep square logic
+        if (m.is_double_pawn_move){
+            int d = (m.end-m.start < 0) ? -8 : 8;
+            ep_sq.push_back(m.end+d);
         }
-        // update all piece boards
-        all_white_pieces = 0;
-        all_black_pieces = 0;
-        empty_squares = 0;
-        for(int i = 0; i<=5 ; i++){
-            all_white_pieces |= bitboards[i];
+        else{
+            ep_sq.push_back(-1);
         }
-        for(int i = 6; i<=11 ; i++){
-            all_black_pieces |= bitboards[i];
-        }
-        empty_squares = ~(all_black_pieces|all_white_pieces);
     }
 
 };
@@ -270,7 +276,10 @@ class Board{
 
 int main(){
     Board b = Board();
+    // b.print_bitboards();
     b.print();
-    cout << endl;
+    // b.make_move(Move(0, 16));
+    // b.print();
+    // b.print_bitboards();
     return 0;
 }
